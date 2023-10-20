498bde61cda8cfe6761d0397aa70304cdaf05932
adam.dziedzic <adam.dziedzic@allegro.com>
2023-10-20 16:27:22 +0200

    #65 - we are safe now

---
+++ src/main/java/ghstats/api/achievements/impl/WeAreSareNow.java
@@ -0,0 +1,38 @@
+package ghstats.api.achievements.impl;
+
+import ghstats.api.achievements.api.AchievementUnlocked;
+import ghstats.api.achievements.api.UnlockableAchievement;
+import ghstats.api.integrations.github.api.GitCommit;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class WeAreSareNow implements UnlockableAchievement {
+
+    static final Pattern PATTERN = Pattern.compile("(\\bsecure?\\b)", Pattern.CASE_INSENSITIVE);
+
+    @Override
+    public String getId() {
+        return "we-are-safe-now";
+    }
+
+    @Override
+    public String getName() {
+        return "We’re Safe Now";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Use the word \"secure\" in a commit message";
+    }
+
+    @Override
+    public Optional<AchievementUnlocked> unlock(List<GitCommit> commits) {
+        return commits.stream()
+                .filter(it -> PATTERN.matcher(it.message()).find())
+                .findFirst()
+                .map(commit -> new AchievementUnlocked(this, commit));
+    }
+}
