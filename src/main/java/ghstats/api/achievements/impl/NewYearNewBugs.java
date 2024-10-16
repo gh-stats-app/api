e22e4413b5d1536dc5838ef6477e912b4d2760af
Michał Wojciechowski <michal.wojciechowski@allegro.com>
2024-10-16 12:26:04 +0200

    #53 - New Year, New Bugs

---
+++ src/main/java/ghstats/api/achievements/impl/NewYearNewBugs.java
@@ -0,0 +1,29 @@
+package ghstats.api.achievements.impl;
+
+import ghstats.api.achievements.api.AchievementUnlocked;
+import ghstats.api.achievements.api.UnlockableAchievement;
+import ghstats.api.integrations.github.api.GitCommit;
+
+import java.util.List;
+import java.util.Optional;
+
+class NewYearNewBugs implements UnlockableAchievement {
+
+    @Override
+    public String getName() {
+        return "new-year";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Commit on Jan 1.";
+    }
+
+    @Override
+    public Optional<AchievementUnlocked> unlock(List<GitCommit> commits) {
+        return commits.stream()
+                .filter(commit -> commit.timestamp().getDayOfYear() == 1)
+                .findAny()
+                .map(commit -> new AchievementUnlocked(this, commit));
+    }
+}
